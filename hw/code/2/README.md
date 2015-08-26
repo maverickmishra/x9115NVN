30a0fc7227aa2f67c330e7330e44df5f78e4793a
Nikhil Anand <anand.nik@gmail.com>
2015-08-26 18:25:10 +0000

    added excercise 3.1 to 3.5

--- hw/code/2/README.md
+++ hw/code/2/README.md
@@ -6,3 +6,52 @@ Collaborators :
 Nikhil Satish Pai
 Vishal Mishra
 Nikhil Anand
+
+# x9115NVN
+CSC 591 (Automated Software Engineering)- Repository for MASE
+
+#Collaborators :
+
+	>> Nikhil Satish Pai(npai)
+
+	>> Vishal Mishra(vmishra)
+
+	>> Nikhil Anand(nanand2)
+
+#Homework 2:
+##Exercise 3.1
+       
+       ![soemTExt](./images/exercise_3_1.png)
+
+##Exercise 3.2
+       
+       ![soemTExt](./images/exercise_3_2.png)
+       
+##Exercise 3.3
+       
+       ![soemTExt](./images/exercise_3_3.png)
+       
+##Exercise 3.4
+
+1)
+       ![soemTExt](./images/exercise_3_4_1.png)
+
+2)
+       ![soemTExt](./images/exercise_3_4_2.png)
+       
+3)
+       ![soemTExt](./images/exercise_3_4_3.png)
+       
+4)
+       ![soemTExt](./images/exercise_3_4_4.png)
+       
+5)
+       ![soemTExt](./images/exercise_3_4_5.png)
+       
+##Exercise 3.5
+       
+       ![soemTExt](./images/exercise_3_5.png)
+
+
+Reference: 
+[1] http://stackoverflow.com/questions/1402048/how-do-i-right-align-numeric-data-in-python
---
+++ hw/code/2/think3.py
@@ -0,0 +1,134 @@
+import os
+
+##Code 2:Coding Homework
+os.system('clear')
+
+#print "**********************"    
+#print "Output of Exercise 3.1"     
+#print "**********************"    
+
+#repeat_lyrics()
+#
+#def print_lyrics():
+#    print "I'm a lumberjack, and I'm okay."
+#    print "I sleep all night and I work all day."
+#
+#def repeat_lyrics():
+#    print_lyrics()
+#    print_lyrics()
+
+os.system('clear')
+print "**********************"    
+print "Output of Exercise 3.2"     
+print "**********************" 
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+repeat_lyrics()
+
+os.system('clear')
+print "**********************"    
+print "Output of Exercise 3.3"     
+print "**********************" 
+
+def right_justify(s):
+    length = len(s)
+    space = 70 - length -1
+    print " "*space,s
+#    print "{:>70}".format(s)
+#    print '%70s' % s
+    
+right_justify("allen")
+right_justify("Nikhil Anand")
+right_justify("Nikhil Pai")
+right_justify("Vishal Mishra")
+
+os.system('clear')
+print "**********************"    
+print "Output of Exercise 3.4"     
+print "**********************" 
+
+def do_twice(f,value):
+    f(value)
+    f(value)
+    
+def print_spam():
+    print 'spam'
+    
+def print_somevalue (value):
+     print value
+
+def print_twice(value):
+    print"print_twice funcation called..."
+    print value
+    print value
+    
+#print_twice("passedValue")
+
+#do_twice(print_twice,"spam")
+
+def do_four(f,value):
+    do_twice(f,value)
+    do_twice(f,value)
+
+do_four(print_twice,"spam")
+
+
+os.system('clear')
+print "**********************"    
+print "Output of Exercise 3.5"     
+print "**********************" 
+
+
+def grid(r,c):
+    poss = 0
+    for i in range (0,r*5+1):
+        if (i % 5 == 0):
+            for j in range (0,c*5+1):
+                if (j % 5 == 0):
+                    print "+ ",
+                else :
+                    print "- ",
+        else :
+            for j in range (0,c*5+1):
+                if (j % 5 == 0):
+                    print "| ",
+                else :
+                    print "  ",  
+        print ""
+
+print "2x2 GRID:" 
+grid(2,2)
+print "4x4 GRID:" 
+grid(4,4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
